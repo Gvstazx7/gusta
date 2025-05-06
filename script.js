7f1e6c5782eb65105c816e194bbd4a9cf18715ae
Gvstazx7 <gvstavic22@gmail.com>
2025-05-06 01:28:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yes').addEventListener('click', function() {
+  alert('Você aceitou! vou ficar mt feliz rsrsrs!');
+});
+
+document.getElementById('no').addEventListener('click', function() {
+  alert('Você não quis, mas ainda vou conquistar esse rabao gigante!');
+});
